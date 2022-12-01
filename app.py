f9dc1a360226ca970c0b96cff90a15527b1613cf
scottzyang <scottzyang@gmail.com>
2022-12-01 15:56:05 -0600

    Add home page route

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ client = MongoClient('localhost', 27017)
 # references to database from MongoDB and the todos collection
 db = client.flask_db
 todos = db.todos
+
+# route serves as Home page
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return render_template('index.html')
